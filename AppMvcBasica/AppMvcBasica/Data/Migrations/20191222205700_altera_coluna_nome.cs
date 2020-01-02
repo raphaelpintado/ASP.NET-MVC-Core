57a89e7bbffc0461792fdef44e9ee9993277f500
Raphael Moraes <raphaelpintado@gmail.com>
2020-01-02 20:36:40 -0300

    app básica em mvc core

---
+++ AppMvcBasica/AppMvcBasica/Data/Migrations/20191222205700_altera_coluna_nome.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppMvcBasica.Data.Migrations
+{
+    public partial class altera_coluna_nome : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nomne",
+                table: "Produtos",
+                newName: "Nome");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Produtos",
+                newName: "Nomne");
+        }
+    }
+}
